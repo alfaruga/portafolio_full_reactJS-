1eb675ce096b566ef9ec3954907037a6ac54367e
Alexis Ruiz <alfaruga@gmail.com>
2020-09-20 18:24:21 -0500

    Updates

---
+++ precache-manifest.46601094687cdcef072b7319dcdf7d82.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e0c43515f82c8ee7708bb0164311877",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eb2bb1a5131ff0fdebbf",
+    "url": "/static/css/main.ba634f85.chunk.css"
+  },
+  {
+    "revision": "31fb1b2697bbc46a66e1",
+    "url": "/static/js/2.b8adb121.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b8adb121.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb2bb1a5131ff0fdebbf",
+    "url": "/static/js/main.ee41eebb.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "ac5331398a2c307e4f03370c0bad49a5",
+    "url": "/static/media/Calculator.ac533139.png"
+  },
+  {
+    "revision": "ef6ca9b9f47d7c545b15c32b05a84755",
+    "url": "/static/media/DrumKit.ef6ca9b9.png"
+  },
+  {
+    "revision": "a829907bbb2ee333c61ba42e71ddf708",
+    "url": "/static/media/Library.a829907b.png"
+  },
+  {
+    "revision": "7d1cf2fb522e467c79d3cba25f09ed60",
+    "url": "/static/media/Product landing page.7d1cf2fb.png"
+  },
+  {
+    "revision": "ff3b401d00a0ee9f570b9e5a9f62f78d",
+    "url": "/static/media/Tic tac toe.ff3b401d.png"
+  },
+  {
+    "revision": "39dc1ccfb44c1a840b422eb8f1dece04",
+    "url": "/static/media/background.39dc1ccf.jpeg"
+  },
+  {
+    "revision": "81a75869140e83deebdd63ef57cee2cb",
+    "url": "/static/media/fotoDePerfil.81a75869.jpg"
+  }
+]);
