2fdde70cb1d3b08022ad6097e2721bfab0661915
Alexis Ruiz <alfaruga@gmail.com>
2020-09-21 09:18:14 -0500

    Trying hash router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Layout from './containers/Layout/Layout';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons';
 import { fas } from '@fortawesome/free-solid-svg-icons'
@@ -8,9 +8,9 @@ import { fas } from '@fortawesome/free-solid-svg-icons'
 function App() {
   library.add(fab, fas)
   return (
-    <BrowserRouter basename="portafolio_full_reactJS-/">
+    <HashRouter basename="/">
       <Layout />
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
